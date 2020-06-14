da57b2f1a47b42158ae69a42627670955a78acb9
MANASRAZA <razaanas1213@gmail.com>
2020-06-14 17:11:05 +0500

    practice

---
+++ app.js
@@ -0,0 +1,472 @@
+// chapter 1 task 1
+
+
+// alert("welcome to javascript")
+
+
+// chapter 1 task 2
+
+
+
+// alert("Error! Please enter a valid password. ")
+
+
+
+
+// chapter 1 task 3
+
+
+// alert("Welcome to JS Land... \n Happy Coding! " )
+
+
+
+
+// chapter 1 task 4
+
+
+// alert("Welcome to JS Land...")
+// alert("Happy Coding!")
+
+
+
+
+// chapter 2 task 1
+
+// q1
+
+// var username ;
+
+
+// chapter 2 task 2
+// Q2
+
+// var myName = "M ANAS RAZA"
+// alert(myName)
+
+
+// chapter 2 task 3
+
+// Q3
+
+// var message = "Hello World";
+// alert(message)
+
+
+
+
+// chapter 2 task 4
+// Q4
+
+// var name = "jhone Doe"
+// var age = "15 years old"
+// var education = "Certifird Mobile Application Development "
+// alert(name)
+// alert(age)
+// alert(education)
+
+
+
+// chapter 2 task 5
+// Q5
+
+// for(var i=0 ; i<=5 ;i++)
+// {
+    
+// for(var j=1 ; j<i ;j++)
+// {
+//   document.write("pizza")
+
+// }
+// document.write("<br>")
+
+// }
+
+
+// chapter 2 task 6
+// Q6
+
+// var email = "ali@gmail.com"
+// alert("My email address is "+ email)
+
+
+// chapter 2 task 7
+// Q7
+// var book = "A smarter \n way to learn JavaScript."
+// alert ("i am trying to learn from the book "+book)
+
+
+// chapter 2 task 8
+// Q8
+
+// document.write("Yah! I can write HTML content through JavaScript")
+
+
+// chapter 2 task 9
+// Q9
+
+// var design  = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// alert(design)
+
+
+
+
+
+// chapter 3 task 1
+
+// Q1
+
+// var age = "15"
+// alert("i am "+age+" years old")
+
+
+
+// chapter 3 task 3
+// Q3
+
+// var birthYear = '1990'
+// document.write("My birth year is "+birthYear +  " <br>  Data type of my decleared variable is number ")
+
+
+// chapter 3 task 4
+// Q4
+
+// var Visitors_name = "  John Doe"
+// var  Product_title = "T-shirt(s)"
+// var  Quantity ="5"
+
+// document.write(Visitors_name.bold() +" ordered "+ Quantity.bold()+" "+ Product_title.bold() +" on XYZ Clothing store.");
+
+
+
+
+
+
+
+// chapter 4 task 1
+
+// Q1
+// var a,b,c = "3";
+
+// chapter 4 task 2
+// Q2
+// legal variable
+// var a ;
+// var ali; 
+// var num ;
+// var a1;
+// var a_q;
+
+// illegal variable 
+// var 1;
+// var 1a;
+// var for;
+// var write;
+// var else ;
+
+
+// chapter 4 task 3
+// Q3
+
+// var a ="Rules for naming JS variables"
+
+// document.write(a.bold()+"<br><br>  Variable names can only contain numbers ,$ and _ ." +"<br> For example $my_1stVariable"+"<br>  Variables must begin with a letter, $ or __ ."+"<br> For example $name, _name or name ." +"<br>  Variable names are case sensitive "+"<br>  Variable names should not be JS keyword ")
+
+
+
+
+
+ // chapter 5 task 1 
+
+// Q1
+
+// var num1 = "3"
+// var num2 = "5"
+// var result ;
+// result=  (+num1) + (+num2);
+// document.write("Sum of 3 and 5 is "+ result)
+
+
+
+// chapter 5 task 2
+// Q2
+// var num1 = "3"
+// var num2 = "5"
+// var sub,divide,multiply,modulus ;
+
+// sub=  (+num1) - (+num2);
+// multiply=  (+num1) * (+num2);
+// devide=  (+num1) / (+num2);
+// modulus=  (+num1) / (+num2)*100;
+
+// document.write("Subtraction of 3 and 5 is "+ sub) 
+// document.write("<br> multiplication of 3 and 5 is "+ multiply)
+// document.write(" <br> division of 3 and 5 is "+ devide)
+// document.write(" <br> modulus of 3 and 5 is "+ modulus)
+
+
+
+// chapter 5 task 3
+// Q3
+// var a ="5";
+
+// document.write("value after variable declaration is : ??")
+
+
+// document.write(" <br> initial value : " + a )
+// a= (+a)+1;
+
+// document.write(" <br> value after increment is : " + a )
+
+// a=(+a)+7
+// document.write(" <br> value after addition is : " + a )
+
+// a=(+a)-1
+// document.write(" <br> value after decrement  is : " + a )
+
+// a=(+a)%3
+
+// document.write(" <br>The  remainder is   : " + a )
+
+
+
+
+// chapter 5 task 4
+// Q4
+
+// var one_ticket  = "600";
+//  var five_ticket = one_ticket * 5; 
+// document.write("total cost to buy 5 tickets to a movie is " +five_ticket+"PKR")
+
+
+// chapter 5 task 5
+// // Q5
+
+// document.write("table of 2 <br><br> ")
+// for (var i=1 ;i<=10 ;i++)
+// {
+    
+//     document.write("2" + "x"  + i + "=" + 2*i +"<br>");
+// }
+
+
+
+// chapter 5 task 6
+// Q6
+
+// function convert(degree) {
+//     var x;
+//     if (degree == "C") {
+//       x = document.getElementById("c").value * 9 / 5 + 32;
+//       document.getElementById("f").value = Math.round(x);
+//     } else {
+//       x = (document.getElementById("f").value -32) * 5 / 9;
+//       document.getElementById("c").value = Math.round(x);
+//     }
+//   }  
+
+
+
+// chapter 5 task 7
+// Q7
+// var price_of_item1= "650"
+// var price_of_item2= "100"
+// var Ordered_quantity_of_item1 = price_of_item1 * 3;
+// var total_Ordered_quantity_of_item1 =Ordered_quantity_of_item1 ;
+// var Ordered_quantity_of_item2 =price_of_item2 * 7;
+// var total_Ordered_quantity_of_item2 =Ordered_quantity_of_item2 ;
+
+// var Shipping_charges ="100"; 
+// var total_cost = (+total_Ordered_quantity_of_item1) + (+total_Ordered_quantity_of_item2) +(+Shipping_charges) ;
+// var head = "Shopping Cart"
+
+// document.write(head.bold())
+ 
+// document.write(" <br><br>price of item1 is 650")
+// document.write("<br> Quantity of item1 is 3")
+// document.write(" <br>price of item2 is 100")
+// document.write(" <br>Quantity of item2 is 7")
+// document.write(" <br>Shipping charges is 100")
+// document.write(" <br><br>Total cost of your order is "+ total_cost)
+
+
+
+
+
+
+// chapter 5 task 8
+// Q8
+
+// var Total_marks= "1100"
+// var Marks_obtained ="900"
+// var percentage = Marks_obtained / Total_marks *100;  
+
+// document.write("<br>Total marks:" + Total_marks)
+// document.write("<br>Marks obtained :" + Marks_obtained)
+// document.write("<br>Percentage:"  + percentage +"%")
+
+
+
+
+// chapter 5 task 10
+// Q10
+
+// var num = "5";
+// num= (+num)+5;
+//  num= (+num)*10;
+//  num = (+num)/2;
+// document.write(num)
+
+
+// chapter 5 task 11
+// Q11
+
+// var a = "Age Calculator"
+// var birth_year  = "1992"
+// var current_year = "2020"
+// var age = (+current_year) - (+birth_year)
+
+// document.write(a.bold()+"<br>"+"<br>" +"Current_year:"+current_year +"<br>"+"Birth_year:"+birth_year+"<br>"+" Your age:"+age   )
+
+
+
+
+
+// chapter 5 task 12
+// Q12
+// var head = "the geometrizer"
+// var r = "5"
+// var circumference =  2 * 3.142 * r
+// var area = 3.142 * r**2 
+
+// document.write(head.bold()+" <br><br> radius of cirle : "+r +"  <br> the circumference is :"+circumference+"  <br>the area is :"+area)
+
+
+
+
+// chapter 5 task 13
+// Q13
+
+// document.write("<h1>The Life Supply Calculator</h1>")
+// var favorite_snack = "prince" 
+// var current_age = "10"
+// var estimated_maximum_age = "50" 
+// var estimated_amount_per_day = "2"
+// total= ((+estimated_maximum_age) - (+current_age)) * (+estimated_amount_per_day)  
+// document.write("<br>favorite_snack:  " +  favorite_snack)
+// document.write(" <br>current_age  "+current_age)
+// document.write(" <br>estimated_maximum_age  "+estimated_maximum_age)
+// document.write("<br>estimated_amount_per_day  "+estimated_amount_per_day)
+// document.write("<br>You will need "+ total+ "   prince biscuit to last you until the ripe old age 50")
+
+
+
+
+
+// chapter 6 task 1
+
+// Q1
+
+// var a= "5"
+
+// document.write("the value of a is :"+a)
+
+// a=++a;
+// document.write("<br><br>the value of ++a is :"+a)
+// document.write("<br> now the value of  a is :"+a)
+
+
+
+// a=a++;
+// document.write("<br><br>the value of a++ is :"+a)
+// document.write("<br> now the value of  a is :"+a)
+
+
+
+
+
+// a=--a;
+// document.write("<br><br>the value of --a is :"+a)
+// document.write("<br> now the value of  a is :"+a)
+
+
+
+
+// a=a--;
+// document.write("<br><br>the value of a-- is :"+a)
+// document.write("<br> now the value of  a is :"+a)
+
+
+
+
+
+// chapter 6 task 2
+// Q2
+// var a ="2" 
+// var b ="1" 
+// var result = --a - --b + ++b + b--;
+// document.write(result)
+
+
+
+// chapter 6 task 3
+// Q3
+// var name=prompt ("enter your name ")
+// document.write("<br>welcome "+name)
+
+
+
+// chapter 6 task 4
+// Q4
+
+// var a = prompt("enter a number ")
+
+
+//  for (var i=1 ;i<=10 ;i++)
+// {
+    
+//     document.write(a + "x"  + i + "=" + a*i +"<br>");
+//  }
+
+
+
+// chapter 6 task 5
+// Q5
+// var sub1= prompt("enter sub 1")
+// var sub2= prompt("enter sub 2")
+// var sub3= prompt("enter sub 3")
+
+// var total = "100";
+// var grandtotal = "300";
+//  var sub1obtmarks = prompt("enter "+sub1+"marks")
+//  var sub2obtmarks = prompt("enter "+sub2+"marks")
+//  var sub3obtmarks= prompt("enter "+sub3+"marks")
+ 
+// total_obt_marks = sub1obtmarks + sub2obtmarks + sub3obtmarks 
+
+// percentage= sub1obtmarks / total *100;
+
+// document.write("sub1 "+sub1 +"total "+total+"sub1obtmarks "+sub1obtmarks+"percentage "+percentage)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
